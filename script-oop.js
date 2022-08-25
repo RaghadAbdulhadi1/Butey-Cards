5b0b9a9acc9b7124f9fb26face9a123b962188f2
Raghad Abdulhadi <Raghad.Abdulhadi@optimumpartners.co>
2022-08-25 15:34:07 +0300

    fix : oop applied

---
+++ script-oop.js
@@ -0,0 +1,159 @@
+class FetchData {
+  constructor() {
+    this.url = "https://jsonplaceholder.typicode.com/photos";
+    this.apiResponse = [];
+    this.jsonFormat = {};
+    this.numberOfObjects = 10;
+  }
+  async fetchData() {
+    this.apiResponse = await fetch(this.url);
+    this.jsonFormat = await this.apiResponse.json();
+    return this.jsonFormat.slice(0, this.numberOfObjects);
+  }
+  getColorsNames() {
+    return {
+      "92c952": "Mantis",
+      "771796": "Seance",
+      "24f355": "Malachite",
+      "d32776": "Cerise",
+      "f66b97": "Froly",
+      "56a8c2": "Fountain Blue",
+      "b0f7cc": "Ice Cold",
+      "54176f": "Honey Flower",
+      "51aa97": "Tradewind",
+      "810b14": "Monarch",
+    };
+  }
+}
+
+class CardsTrack {
+  constructor() {
+    this.colorsCount = document.createElement("h2");
+    this.colorsName = document.createElement("h2");
+
+    this.colorsCount.setAttribute("class", "colors-count");
+    this.colorsName.setAttribute("class", "colors-name");
+
+    this.colorsCount.innerText = "Count of Selected Cards: ";
+    this.colorsName.innerText = "Selected Colors: ";
+  }
+}
+
+class PageHeader extends CardsTrack {
+  constructor() {
+    super();
+    this.header = document.createElement("header");
+    this.title = document.createElement("h1");
+
+    this.title.setAttribute("class", "title");
+    this.header.setAttribute("class", "page-header");
+
+    this.title.innerText = "Lorem Ipsum";
+
+    this.header.appendChild(this.title);
+    this.header.appendChild(this.colorsCount);
+    this.header.appendChild(this.colorsName);
+
+    this.body = document.querySelector("body");
+    this.body.appendChild(this.header);
+  }
+  getHeader() {
+    return this.header;
+  }
+}
+
+class Card extends CardsTrack {
+  constructor(card, cardName, colorslist) {
+    super();
+    this.card = card;
+
+    this.card = document.createElement("div");
+    this.cardImage = document.createElement("img");
+    this.cardTitle = document.createElement("h3");
+    this.cardDescription = document.createElement("p");
+    this.cardButton = document.createElement("button");
+
+    this.card.setAttribute("class", "card");
+    this.cardImage.setAttribute("src", card.url);
+    this.cardTitle.setAttribute("class", "card-title");
+    this.cardDescription.setAttribute("class", "card-description");
+    this.cardButton.addEventListener("click", this.onClick);
+
+    this.cardImage.textContent = card.url;
+    this.cardTitle.textContent = card.title;
+    this.cardDescription.textContent = card.title;
+    this.cardButton.textContent = "Button";
+
+    this.card.appendChild(this.cardImage);
+    this.card.appendChild(this.cardTitle);
+    this.card.appendChild(this.cardDescription);
+    this.card.appendChild(this.cardButton);
+
+    this.hexCode = card.url.substr(card.url.length - 6);
+    this.isClicked = false;
+    this.cardName = cardName;
+    this.colorsNameList = colorslist;
+  }
+  onClick = () => {
+    this.isClicked = !this.isClicked;
+    if (this.isClicked) {
+      this.colorsNameList.push(this.cardName[this.hexCode]);
+      this.cardsCounter++;
+      this.cardButton.style.backgroundColor = "#669bbc";
+      this.card.style.boxShadow = "0px 5px 9px 3px rgb(171, 171, 172)"
+    } else {
+      this.colorsNameList.splice(
+        this.colorsNameList.indexOf(this.cardName[this.hexCode]),
+        1
+      );
+      this.cardsCounter--;
+      this.cardButton.style.backgroundColor = "#003049";
+      this.card.style.boxShadow = "5px 5px 4px 3px rgb(255, 255, 255)"
+    }
+    this.colorsCount = document.querySelector('.colors-count');
+    this.colorsCount.innerHTML = "Count of Selected Cards: " + this.colorsNameList.length;
+    this.colorsName = document.querySelector('.colors-name');
+    this.colorsName.innerHTML = "Selected Colors: " + this.colorsNameList;
+  };
+  getCard() {
+    return this.card;
+  }
+}
+
+class Cards {
+  constructor() {
+    this.body = document.querySelector("body");
+    this.cards = document.createElement("div");
+
+    this.cards.setAttribute("class", "cards");
+
+    this.body.appendChild(this.cards);
+
+    this.fetchedData = new FetchData();
+    this.colorsName = this.fetchedData.getColorsNames();
+    this.cardsData = [];
+    this.colorsNameList = [];
+  }
+  async renderCards() {
+    this.cardsData = await this.fetchedData.fetchData();
+    this.cardsData?.forEach((card) => {
+      this.cards.appendChild(
+        new Card(
+          card,
+          this.colorsName,
+          this.colorsNameList
+        ).getCard()
+      );
+    });
+  }
+}
+
+class RenderPage {
+  constructor() {
+    this.header = new PageHeader();
+    this.body = new Cards();
+    this.header.getHeader();
+    this.body.renderCards();
+  }
+}
+const render = new RenderPage();
--- script.js
+++ script.js
@@ -1,20 +1,115 @@
+class CardDetails {
+  constructor() {
+    this.colorsCount = document.createElement("h2");
+    this.colorsName = document.createElement("h2");
+    this.colorsCount.setAttribute("class", "colors-count");
+    this.colorsName.setAttribute("class", "colors-name");
+    this.colorsCount.innerText = "Count of Selected Cards: ";
+    this.colorsCount.innerText = "Selected Colors: ";
+  }
+
+  getCardDetails() {
+    return this.colorsCount, this.colorsName;
+  }
+}
+
+class Header {
+  static title = document.createElement("h1");
+  constructor() {
+    Header.title.setAttribute("class", "title");
+    Header.title.innerText = "Lorem Ipsum";
+  }
+}
+
+class CardItems {
+  static card = document.createElement("div");
+  static cardImage = document.createElement("img");
+  static cardTitle = document.createElement("h3");
+  static cardDescription = document.createElement("p");
+  constructor() {
+    this.cardButton = document.createElement("button");
+    CardItems.card.setAttribute("class", "card");
+    CardItems.cardImage.setAttribute(
+      "src",
+      CardItems.fetchedData.cardsData.url
+    );
+    CardItems.cardTitle.setAttribute("class", "card-title");
+    CardItems.cardDescription.setAttribute("class", "card-description");
+    this.cardButton.addEventListener("click", this.clickButton());
+    CardItems.cardImage.textContent = CardItems.fetchedData.cardsData.url;
+    CardItems.cardTitle.textContent = CardItems.fetchedData.cardsData.title;
+    this.cardDescription.textContent = CardItems.fetchedData.cardsData.title;
+    this.cardButton.textContent = "Button";
+    CardItems.card.appendChild(cardImg);
+    CardItems.card.appendChild(cardTitle);
+    CardItems.card.appendChild(cardDescription);
+    CardItems.card.appendChild(cardButton);
+    /////////////////////////////////////
+    // CardItems.cards.appendChild(card);
+    /////////////////////////////////////
+  }
+}
+
+class CardButton extends CardDetails {
+  constructor() {
+    this.colorsCounter = 0;
+    this.colorsNameList = [];
+    this.cardsCounter = 0;
+    this.colorsCount += this.colorsCounter;
+    this.colorsName += this.colorsName;
+    this.isClicked = false;
+    // this.selectedColor = colorsName[hexCode];
+  }
+  clickButton() {
+    this.isClicked = !this.isClicked;
+    if (this.isClicked) {
+      // this.colorsNameList.push(selectedColor);
+      this.cardsCounter++;
+      this.cardButton.style.backgroundColor = "#669bbc";
+    } else {
+      // this.colorsNameList.splice();
+      this.cardsCounter--;
+      this.cardButton.style.backgroundColor = "#003049";
+    }
+  }
+}
+
+class Cards extends CardButton {
+  static cards = document.createElement("div");
+
+  constructor() {
+    super();
+
+    this.fetchedData = new FetchData();
+    this.cardsDetails = [];
+    this.hexCode = "";
+  }
+
+  drawCards() {
+    this.cardsDetails = this.fetchedData.fetchData();
+    this.cardsDetails.forEach((card) => {
+      this.hexCode = card.url.substr(card.url.length - 6);
+    });
+  }
+}
+
 const cards = document.getElementById("cards");
 const colorsName = {
   "92c952": "Mantis",
-  "771796": "Seance",
+  771796: "Seance",
   "24f355": "Malachite",
-  "d32776": "Cerise",
-  "f66b97":"Froly",
+  d32776: "Cerise",
+  f66b97: "Froly",
   "56a8c2": "Fountain Blue",
-  "b0f7cc": "Ice Cold",
+  b0f7cc: "Ice Cold",
   "54176f": "Honey Flower",
   "51aa97": "Tradewind",
-  "810b14": "Monarch"
-}
+  "810b14": "Monarch",
+};
 async function fetchColorsJSON() {
   const response = await fetch("https://jsonplaceholder.typicode.com/photos");
   const colors = await response.json();
-  return colors.slice(0,10);
+  return colors.slice(0, 10);
 }
 
 let counter = 0;
@@ -55,7 +150,7 @@ async function addColorsToCards() {
           counter = counter - 1;
           cardButton.style.backgroundColor = "#003049";
         }
-        count.innerHTML = "Count of Selected Cards: " + counter;
+        count.innerHTML = "Count of Selected Cards: " + colorNames.length();
         cardsColors.innerHTML = "Selected Colors:" + colorNames;
       })
     );
