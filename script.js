a47ea45e58f2bd50e403d320a72c436e11a01ca9
Raghad Abdulhadi <Raghad.Abdulhadi@optimumpartners.co>
2022-08-31 17:33:01 +0300

    initialize

---
+++ script.js
@@ -0,0 +1,19 @@
+class EmailFeild {
+    constructor() {
+        this.email = document.createElement('div');
+    }
+}
+
+class PasswordFeild {
+    constructor() {
+        this.password = document.createElement('div');
+    }
+}
+
+
+class LogInForm {
+    constructor() {
+        this.container = document.getElementById('container');
+        this.logInForm = document.createElement('div');
+    }
+}
