9cf9239974707727e39f9bbcb58503fb35549f1a
Raghad Abdulhadi <Raghad.Abdulhadi@optimumpartners.co>
2022-09-04 01:55:46 +0300

    register and login form structure

--- script.js
+++ script.js
@@ -1,74 +1,114 @@
-class Image {
-  constructor(url) {
-    this.url = url;
-  }
-}
-class FormTitle {
-  constructor() {
-    this.title = "";
-  }
-}
+// class Image {
+//   constructor(url) {
+//     this.url = url;
+//   }
+// }
 
-class FieldIcon {
-  constructor() {}
-}
+// // class FormTitle {
+// //   constructor(title) {
+// //     this.title = document.createElement("div");
+// //     this.title.setAttribute("class", "title");
+// //     this.title.innerText = title; 
+// //   }
+// //   get getTitle() { 
+// //     return this.title; 
+// //   }
+// // }
 
-class UserNameFeild {
-  constructor() {
-    this.userName = document.createElement("div");
-  }
-  get userName() {
-    return this.userName;
-  }
-}
+// // class DropDownFeild {
+// //   constructor() {
 
-class EmailFeild {
-  constructor() {
-    this.email = document.createElement("div");
-  }
-  isValidated() {}
-  isUnique() {}
-}
+// //   }
+// // }
 
-class PasswordFeild {
-  constructor() {
-    this.password = document.createElement("div");
-  }
-  get password() {
-    return this.password;
-  }
-  set password(newPassword) {
-    this.password = newPassword;
-  }
-  isMatched() {}
-}
+// // class RadioFeild {
+// //   constructor() {
+// //   }
+// // }
 
-class SubmitButton {
-  constructor() {}
-  onSubmit() {}
-}
+// class FormInputBox {
+//   constructor(iconClassName, placeholder) {
+//     this.inputBox = document.createElement("div");
+//     this.inputBox.setAttribute("class", "input-box");
+    
+//     this.icon = document.createElement('i');
+//     this.icon.setAttribute('class', iconClassName);
+    
+//     this.inputField = document.createElement('input');
+//     this.inputField.type = 'text';
+//     this.inputField.placeholder = placeholder;
+    
+//     this.inputBox.appendChild(this.icon);
+//     this.inputBox.appendChild(this.inputField);
+//   }
+// }
 
-class LogInForm {
-  constructor() {
-    this.container = document.getElementById("container");
-    this.logInForm = document.createElement("div");
-  }
-  register() {}
-}
+// class Button {
+//   constructor(buttonText) {
+//     this.buttonContainer = document.createElement("div");
+//     this.buttonContainer.setAttribute("class", "button input-box");
+//     this.button = document.createElement('button');
+//     this.button.type = 'submit';
+//     this.button.value = 'Submit';
+//     this.button.innerHTML = buttonText;
+//     this.buttonContainer.appendChild(this.button);
+//     console.log(this.button);
+//   }
+//   // onSubmit() {}
+// }
 
-class RegistrationForm {
-  constructor() {
-    this.container = document.getElementById("container");
-    this.logInForm = document.createElement("div");
-  }
-  registered() {}
-}
+// class FormStructure {
+//   constructor(formType, title) {
+//     this.title = document.createElement("div");
+//     this.title.setAttribute("class", "title");
+//     this.title.innerText = title; 
 
-class RenderPage {
-    constructor() {
-        const registration = new RegistrationForm();
-        const logIn = new LogInForm();
-    }
-}
+//     this.formContainer = document.createElement("div");
+//     this.formContainer.setAttribute("class", "form-container");
+//     this.form = document.createElement("form");
+//     this.form.action = "#";
+//     this.formContent = document.createElement("div");
+//     this.formContent.setAttribute("class", "form-content");
+//     this.formTemplate = document.createElement("div");
+//     this.formTemplate.setAttribute("class", formType);
+//     this.formContainer.appendChild(this.form);
+//     this.form.appendChild(this.formContent);
+//     this.formContent.appendChild(this.formTemplate);  
+//   }
+// }
 
-const renderPage = new RenderPage();
+// class LogInForm {
+//   constructor() {
+//     this.container = document.getElementById("container");
+//     this.formType = new FormStructure("login-form");
+//     this.email = new FormInputBox("fa-solid fa-envelope", "Enter your email").inputBox;
+//     this.password = new FormInputBox("fa-solid fa-lock", "Enter your password").inputBox;
+//     this.submit = new Button("Submit").buttonContainer;
+
+//     this.formTemplate.appendChild(this.formType.formContainer);
+//     this.formTemplate.appendChild(this.email);
+//     this.formTemplate.appendChild(this.password);
+//     this.formTemplate.appendChild(this.submit);
+//     this.form.appendChild(this.formContent);
+//     this.formContent.appendChild(this.formTemplate)
+//     this.container.appendChild(this.formTemplate);
+//   }
+//   // register() {}
+// }
+
+// // class RegistrationForm {
+// //   constructor() {
+// //     this.container = document.getElementById("container");
+// //     this.logInForm = document.createElement("div");
+// //   }
+// //   registered() {}
+// // }
+
+// class RenderPage {
+//     constructor() {
+//         // const registration = new RegistrationForm();
+//         const logIn = new LogInForm();
+//     }
+// }
+
+// const renderPage = new RenderPage();
